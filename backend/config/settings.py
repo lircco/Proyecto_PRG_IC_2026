0b56617fdfb39d33d5975fade6f6ba0e88f51737
maxirios <158509311+maxirios@users.noreply.github.com>
2026-04-16 03:27:31 -0300

    feat: crear un primer endpont

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -123,6 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
